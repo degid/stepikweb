78ed61e41333f4daf7c483e90bfd87dddabeb50e
Aleksander Shigin <sdegid@gmail.com>
2019-02-06 00:15:13 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#sudo unlink /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/site-enabled/default
-sudo ln -sf /home/nyan/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart

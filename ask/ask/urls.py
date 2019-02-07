e0e92c4d6ea0f1f8f8dce3fc68d75c3e72c4a667
Aleksander Shigin <sdegid@gmail.com>
2019-02-08 02:16:10 +0300

    ts 6 fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,12 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/$', 'qa.views.test'),
-    url(r'^signup/$', 'qa.views.test'),
-    url(r'^question/\d+$', 'qa.views.test'),
-    url(r'^ask/$', 'qa.views.test'),
-    url(r'^popular/$', 'qa.views.test'),
-    url(r'^new/$', 'qa.views.test'),
+    url(r'^', include("qa.urls")),
     #url(r'^admin/', admin.site.urls),
 ]

5f15972e91f3f0a26c851e142c477f7e379681da
Aleksander Shigin <sdegid@gmail.com>
2019-02-11 03:50:35 +0300

    ask fx

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
+from qa.views import test
 
-urlpatterns = [
-    url(r'', 'qa.views.test'),
-    url(r'login/$', 'qa.views.test'),
-    url(r'signup/$', 'qa.views.test'),
-    url(r'question/\d+$', 'qa.views.test'),
-    url(r'ask/$', 'qa.views.test'),
-    url(r'popular/$', 'qa.views.test'),
-    url(r'new/$', 'qa.views.test'),
+urlpatterns = patterns('qa.views',
+    url(r'^$', test, name='qa'),
+    url(r'^login/$', test, name='login'),
+    url(r'^signup/$', test, name='signup'),
+    url(r'^question/(\d+)/$', test, name='question'),
+    url(r'^ask/$', test, name='ask'),
+    url(r'^popular/$', test, name='popular'),
+    url(r'^new/$', test, name='new'),
     #url(r'admin/', admin.site.urls),
-]
+    )

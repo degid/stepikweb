5d64535a8964e5ad714dee1cc2594cbf642333ad
degid <sdegid@gmail.com>
2019-04-30 01:39:15 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo rm -rf /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/ask.py   /etc/gunicorn.d/ask.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart

e0bfba134e98b6d6ef6770bc34a893a469d4e914
degid <sdegid@gmail.com>
2019-05-02 20:00:45 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from qa.models import Question, Answer
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def index(request):
+    try:
+        page = int(request.GET.get("page"))
+    except ValueError:
+        page = 1
+    except TypeError:
+        page = 1
+    questions = Question.objects.all().order_by('-id')
+    paginator = Paginator(questions, 10)
+    page = paginator.page(page)
+
+    return render(request, 'index.html',
+                  {'title': 'Latest',
+                   'paginator': paginator,
+                   'questions': page.object_list,
+                   'page': page,
+                   'user': request.user,
+                   'session': request.session, })
+
+
+def popular(request):
+    try:
+        page = int(request.GET.get("page"))
+    except ValueError:
+        page = 1
+    except TypeError:
+        page = 1
+    questions = Question.objects.all().order_by('-rating')
+    paginator = Paginator(questions, 10)
+    page = paginator.page(page)
+
+    return render(request, 'index.html',
+                  {'title': 'Popular',
+                   'paginator': paginator,
+                   'questions': page.object_list,
+                   'page': page,
+                   'user': request.user,
+                   'session': request.session, })
+
+def question(request, num,):
+    try:
+        q = Question.objects.get(id=num)
+    except Question.DoesNotExist:
+        raise Http404
+    #if request.method == "POST":
+    #    form = AnswerForm(request.POST)
+    #    if form.is_valid():
+    #        form._user = request.user
+    #        _ = form.save()
+    #        url = q.get_url()
+    #        return HttpResponseRedirect(url)
+    #else:
+    #form = AnswerForm(initial={'question': q.id})
+
+    #return render(request, 'question.html', {'question': q,
+    #                                         'form': form,
+    #                                         'user': request.user,
+    #                                         'session': request.session, })
+    return render(request, 'question.html', {'question': q, })

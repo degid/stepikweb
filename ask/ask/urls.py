6701ea8f5eee730e60a88a4c2827d52ebc32aa9a
degid <sdegid@gmail.com>
2019-05-02 21:33:58 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
-from qa.views import test, index, popular, question, ask
+from qa.views import test, index, popular, question, ask, login_view, signup
 
 urlpatterns = [
     url(r'^$', index),
-    url(r'^login/$', test),
-    url(r'^signup/$', test),
+    url(r'^login/$', login_view),
+    url(r'^signup/$', signup),
     url(r'^ask/$', ask),
     url(r'^popular/$', popular),
     url(r'^new/$', test),
